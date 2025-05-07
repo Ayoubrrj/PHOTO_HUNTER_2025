3ac7578f9a956c5f86128c11ce753aa028554a56
Ayoubrrj <ayoubberrajaapro@gmail.com>
2025-05-07 13:45:20 +0200

    Route photos.index
    Avec le partial _index

--- app/routers/index.php
+++ app/routers/index.php
@@ -1,10 +1,18 @@
 <?php
 // ROUTER PRINCIPAL
 
-
+// ROUTE DES PHOTOS
+// PATTERN: /?photos
+// CTRL: photosController
+// ACTION: indexAction
+if (isset($_GET['photos'])) :
+    include_once '../app/controllers/photosController.php';
+\App\Controllers\PhotosController\indexAction($connexion);
 // ROUTE PAR DÉFAUT
 // PATTERN: /
 // CTRL: pagesController
 // ACTION: homeAction
+else:
 include_once '../app/controllers/pagesController.php';
 \App\Controllers\PagesController\homeAction($connexion);
+endif;
